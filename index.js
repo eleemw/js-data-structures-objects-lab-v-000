1a343d902df392bb20fcc57327d603824d3c6d8f
Erica Murdock-Waters <ericahpa@gmail.com>
2019-09-26 04:27:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
   const newDriver = { ...driver};  //make a copy of original object
-
   newDriver[key] = value;  //add new key value pair
-
   return newDriver;
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
-
   return driver;
 }
 
