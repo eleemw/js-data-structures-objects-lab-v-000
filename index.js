7aab0d235f5df24eebabac31578cbc44bffd9fbf
Erica Murdock-Waters <ericahpa@gmail.com>
2019-09-26 04:13:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+const driver = {};
+const someDriver = { someKey: "someValue" };
+
+function updateDriverWithKeyAndValue(someDriver, someKey, someValue) {
+  const newDriver = { ...someDriver};  //make a copy of original object
+
+  newDriver[someKey] = someValue;  //add new key value pair
+
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(someDriver, someKey, someValue) {
+  someDriver[someKey] = someValue;
+
+  return someDriver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+
+  const newDriver = Object.assign({}, driver);
+
+  newDriver;
+
+  delete newDriver.key;
+
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  driver.pop['key'];
+  return driver;
+}
+
+
+
+//function destructivelyUpdateDriverWithKeyAndValue(driver, someKey, someValue){
+  //driver.someKey['someValue'];
+  //driver;
+//};
