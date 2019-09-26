9729676b0ab501907e655a05070ec0a3dc252c24
Erica Murdock-Waters <ericahpa@gmail.com>
2019-09-26 04:31:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function deleteFromDriverByKey(driver, key) {
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
-  driver = delete driver['key'];
+  delete driver['key'];
   driver;
 }
